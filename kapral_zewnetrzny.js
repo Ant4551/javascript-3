eb18adbe6162af518a2776e3f7a5bba5ea6215d7
Ant <A.kapral@outlook.com>
2025-09-21 20:56:38 +0200

    Add files via upload

---
+++ kapral_zewnetrzny.js
@@ -0,0 +1,5 @@
+// Zewnętrzny plik JavaScript dla Antoni Kapral
+// Pierwszy wiersz - dla numeru 10: kolor 7, Pogrubiony: Tak, Przekreślony: Nie, Rozmiar: 5
+document.write("ostatnia modyfikacja strony".fontcolor("#800080").bold().fontsize(5) + "<br>");
+// Drugi wiersz - dla numeru 10: kolor 6, Pogrubiony: Nie, Przekreślony: Tak, Rozmiar: 6
+document.write(document.lastModified.fontcolor("#FF0000").strike().fontsize(6));
